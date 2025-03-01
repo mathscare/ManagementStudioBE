8ea7d5d0054cc9ce19d96287cbffb6ea68ba434c
TDVamit <102683989+TDVamit@users.noreply.github.com>
2025-03-01 13:46:18 +0530

    1 march 2025

--- app/core/settings.py
+++ app/core/settings.py
@@ -3,15 +3,13 @@ from pydantic_settings import BaseSettings
 import os
 
 class Settings(BaseSettings):
-    secret_key: str = "default_secret"
+    secret_key: str = os.getenv("SECRET_KEY")
     algorithm: str = "HS256"
     access_token_expire_minutes: int = 30
-    database_url: str = "sqlite:///./local_dev.db"
-    AWS_ACCESS_KEY_ID: str
-    AWS_SECRET_ACCESS_KEY: str
-    AWS_S3_BUCKET: str
+    database_url: str = os.getenv("sqlite:///./local_dev.db")
+    AWS_ACCESS_KEY_ID: str = os.getenv("AKIA5IJOW2W3NRO2FT4U")
+    AWS_SECRET_ACCESS_KEY: str = os.getenv("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")
+    AWS_S3_BUCKET: str = os.getenv("gpdashboard-events-attachments")
 
-    class Config:
-        env_file = ".env"
 
 settings = Settings()
