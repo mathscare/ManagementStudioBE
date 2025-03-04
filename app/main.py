a1f31838f3f6d985e97a0acb864c2e51a690cd17
TDVamit <102683989+TDVamit@users.noreply.github.com>
2025-03-04 16:34:28 +0530

    cors origins allowed

--- app/main.py
+++ app/main.py
@@ -1,10 +1,21 @@
 from fastapi import FastAPI
 from app.api.v1.endpoints import auth, user, events
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI(
     title="MathscareDashbaordBE",
     description="Backend Docs For The Dashboard",
     version="1.0.0",
+)
+
+# Allow all origins
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows requests from any origin
+    allow_credentials=True,  # Allows cookies and authentication headers
+    allow_methods=["*"],  # Allows all HTTP methods (GET, POST, PUT, DELETE, etc.)
+    allow_headers=["*"],  # Allows all headers (Authorization, Content-Type, etc.)
 )
 
 app.include_router(auth.router, prefix="/auth", tags=["Auth"])
